64be76d0c3454826c246425ce95f8af81de7c402
Kaan <44790531+kh4xi@users.noreply.github.com>
2023-04-09 17:37:59 +0300

    Update max-mem.sh

--- max-mem.sh
+++ max-mem.sh
@@ -1,3 +1,7 @@
 # Set systemd hard limit
 systemd-run --user --scope -p MemoryMax=<memorylimit> \
   -p MemorySwapMax=<swaplimit> <command>
+
+# CPU Quota half
+systemd-run --user --scope -p CPUQuota=50% -p MemoryMax=<memorylimit> \
+  -p MemorySwapMax=<swaplimit> <command>
